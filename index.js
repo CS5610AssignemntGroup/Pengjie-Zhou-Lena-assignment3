968b6db243c902b5a48bf9bb56b530c4bf31a02c
crispgit <zhoupj1215@hotmail.com>
2020-11-28 15:29:37 -0800

    add eslint, prettier, config file

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({hi: 'there'});
-})
+    res.send({ hi: 'there' });
+});
 
 const port = process.env.PORT || 5610;
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+    console.log(`Listening at http://localhost:${port}`);
 });
