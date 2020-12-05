cb38cc6130ce4be285e332e61516849ed463aa66
LenaLeona <cctnsw@gmail.com:q!:wq>
2020-12-04 22:15:31 -0800

    add change

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,7 +5,8 @@ export default function Home() {
         <div>
             <p>Welcome to URL Shortener</p>
             <p>Please click the menu to create Unbranded or Branded URL</p>
-            //TODO add instructions
+            <p>To get a random shorten url, please use Unbranded tag</p>
+            <p>To get a customised shorten url, or to edit/delete the url, please use Unbranded tag</p>
         </div>
     );
 }
