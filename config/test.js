a24fa0693907e5b111c5b97b03c7d077448b4d69
crispgit <zhoupj1215@hotmail.com>
2020-12-05 17:13:42 -0800

    remove test.js

--- config/test.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI:
-        'mongodb+srv://dbUserWebA3:ZaY318JSqc6OpL9C@cluster0.bvmga.mongodb.net/cs5610assignment3?retryWrites=true&w=majority',
-    baseUrl: 'https://cs5610assignment3.herokuapp.com',
-};
